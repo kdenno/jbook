4c6561d552c0e879c135001c036c32415dd42444
kdenno <kabuyedennis3@gmail.com>
2021-06-28 11:16:27 +0300

    Breaking up onResolve logic with filters

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,22 +10,26 @@ export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        }
+      
+      // handle root entry file of index.js
+    build.onResolve({ filter: /(^index\.js$)/ }, async (args: any) => {
+      return { path: args.path, namespace: 'a' };
+    });
 
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(
-              args.path,
-              'https://unpkg.com' + args.resolveDir + '/'
-            ).href,
-          };
-        }
+    // handle relative paths in a module
+    build.onResolve({ filter: /^\.+\// }, async (args: any) => {
+      return {
+        namespace: 'a',
+        path: new URL(
+          args.path,
+          'https://unpkg.com' + args.resolveDir + '/'
+        ).href,
+      };
+      
+    });
 
+    // handle main file of a module
+    build.onResolve({ filter: /.*/ }, async (args: any) => {
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`,
