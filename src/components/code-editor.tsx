660a386e53e4a6ed9471f4b6f446dbd557467eb4
kdenno <kabuyedennis3@gmail.com>
2021-07-11 20:08:48 +0300

    Handling editor change events

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,22 +1,41 @@
 import MonacoEditor from '@monaco-editor/react';
 import React from 'react';
 
-const CodeEditor = () => {
-    return <MonacoEditor 
-    theme="dark"
-    height="300px"
-    language="javascript"
-    options={{ 
-        wordWrap: 'on', 
+interface codeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+const CodeEditor: React.FC<codeEditorProps> = ({ initialValue, onChange }) => {
+  // editorDidMount prop will pass functions that give us access to the editor
+  const onEditorDidMount = (
+    getCurrentEditorInput: () => string,
+    editorInputListener: any
+  ) => {
+    editorInputListener.onDidChangeModelContent(() => {
+      // update state
+      onChange(getCurrentEditorInput());
+    });
+  };
+
+  return (
+    <MonacoEditor
+      editorDidMount={onEditorDidMount}
+      value={initialValue}
+      theme="dark"
+      height="300px"
+      language="javascript"
+      options={{
+        wordWrap: 'on',
         folding: false,
         lineNumbersMinChars: 3,
         fontSize: 16,
         scrollBeyondLastLine: false,
         showUnused: false,
-        minimap: {enabled: false},
-        automaticLayout: true
-    }} 
+        minimap: { enabled: false },
+        automaticLayout: true,
+      }}
     />
+  );
 };
 
 export default CodeEditor;
