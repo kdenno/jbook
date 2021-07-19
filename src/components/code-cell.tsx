818c52c669af8826cd6784d09e2118a9581de7bd
kdenno <kabuyedennis3@gmail.com>
2021-07-19 10:38:39 +0300

    add debounce logic

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -3,17 +3,20 @@ import CodeEditor from './code-editor';
 import Preview from './preview';
 import codeBundler from '../bundler';
 import Resizable from './resizable';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const CodeCell = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
-
-  const onClick = async () => {
-    const output = await codeBundler(input);
-    setCode(output);
-  };
-
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await codeBundler(input);
+      setCode(output);
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
   return (
     <Resizable direction="vertical">
       <div>
